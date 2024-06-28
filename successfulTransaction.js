4c10b434425c9bfed6b3df97018c117b3acbf2c7
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-28 16:42:35 +0100

    successful transaction

---
+++ successfulTransaction.js
@@ -0,0 +1,34 @@
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs = inputUTXOs;
+        this.outputUTXOs = outputUTXOs;
+    }
+    execute() {
+        const anySpent = this.inputUTXOs.some((x) => x.spent);
+        if (anySpent) {
+            throw new Error("Cannot include a spent UTXO");
+        }
+        let sumInput = 0
+        let sumOutput = 0
+        for (let i = 0; i < this.inputUTXOs.length; i++) {
+            sumInput += this.inputUTXOs[i].amount;
+        }
+        for (let i = 0; i < this.outputUTXOs.length; i++) {
+            sumOutput += this.outputUTXOs[i].amount;
+        }
+        console.log(sumInput)
+        console.log(sumOutput)
+
+        if (sumInput < sumOutput){
+            throw new Error("Your balance is low")
+        }
+
+        this.inputUTXOs.forEach((utxo) => {
+            utxo.spent = true;
+        })
+
+
+    }
+}
+
+module.exports = Transaction;
