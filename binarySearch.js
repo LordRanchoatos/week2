99db49efdc39b6387b49d8f0ecbcb844069e7255
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-28 23:13:34 +0100

    node class

---
+++ binarySearch.js
@@ -0,0 +1,11 @@
+//creating a basic node class
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        
+    }
+}
+
+module.exports = Node;
