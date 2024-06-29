3216dd45a14e4e12614f9dcc63f73925ce35fcb7
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-29 13:03:24 +0100

    Many Layers

--- tree.js
+++ tree.js
@@ -1,21 +1,34 @@
 class Tree {
-  constructor() {
-    this.root = null;
-  }
-
-  addNode = (node) => {
-    if (!this.root) {
-      this.root = node;
+    constructor() {
+        this.root = null;
     }
 
-    if (node.data < this.root.data) {
-      this.root.left = node;
-    }
+    addNode = (node) => {
+        if (!this.root) {
+            this.root = node;
+            return;
+        }
 
-    if (node.data > this.root.data) {
-      this.root.right = node;
+        let current = this.root;
+
+        while (true) {
+            if (node.data < current.data) {
+                // Go left
+                if (!current.left) {
+                    current.left = node;
+                    break;
+                }
+                current = current.left;
+            } else {
+                // Go right
+                if (!current.right) {
+                    current.right = node;
+                    break;
+                }
+                current = current.right;
+            }
+        }
     }
-  };
 }
 
 module.exports = Tree;
