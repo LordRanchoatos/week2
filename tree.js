dac0f5c3a98159e5c5a6006025901d2e2a386846
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-29 09:51:08 +0100

    First Layer

--- tree.js
+++ tree.js
@@ -1,11 +1,21 @@
 class Tree {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  addNode = (node) => {
+    if (!this.root) {
+      this.root = node;
     }
 
-    addNode = function(node) {
-        this.root = node;
-    } 
+    if (node.data < this.root.data) {
+      this.root.left = node;
+    }
+
+    if (node.data > this.root.data) {
+      this.root.right = node;
+    }
+  };
 }
 
 module.exports = Tree;
