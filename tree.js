fa514229a8a03929121ce5f492237df849da93f5
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-06-30 23:52:37 +0100

    tree

--- tree.js
+++ tree.js
@@ -1,34 +1,35 @@
 class Tree {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  addNode = (node) => {
+    if (!this.root) {
+      this.root = node;
+      return;
     }
 
-    addNode = (node) => {
-        if (!this.root) {
-            this.root = node;
-            return;
-        }
+    let current = this.root;
 
-        let current = this.root;
-
-        while (true) {
-            if (node.data < current.data) {
-                // Go left
-                if (!current.left) {
-                    current.left = node;
-                    break;
-                }
-                current = current.left;
-            } else {
-                // Go right
-                if (!current.right) {
-                    current.right = node;
-                    break;
-                }
-                current = current.right;
-            }
+    while (true) {
+      if (node.data < current.data) {
+        // Go left
+        if (!current.left) {
+          current.left = node;
+          break;
         }
+        current = current.left;
+      } else {
+        // Go right
+        if (!current.right) {
+          current.right = node;
+          break;
+        }
+        current = current.right;
+      }
     }
+  };
 }
 
+// module.exports = Tree;
 module.exports = Tree;
