b9118d301df26d56b0be9e3bf59f4865e1c8e5f5
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-01 14:33:58 +0100

    searching through a tree

--- tree.js
+++ tree.js
@@ -3,6 +3,25 @@ class Tree {
     this.root = null;
   }
 
+  hasNode(data) {
+    return this.searchRoot(this.root, data);
+  }
+
+  searchRoot(root, data) {
+    if (!root) {
+      return false;
+    }
+    if (root.data === data) {
+      return true;
+    }
+    if (root.data > data) {
+      return this.searchRoot(root.left, data);
+    }
+    if (root.data < data) {
+      return this.searchRoot(root.right, data);
+    }
+  }
+
   addNode = (node) => {
     if (!this.root) {
       this.root = node;
