8657ae44d61dad3e67a85cb46263db1935ee3d2a
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-03 23:43:43 +0100

    unsigned integers

--- solidity/Contract.sol
+++ solidity/Contract.sol
@@ -6,3 +6,13 @@ contract Contract {
     bool public b = false;
 	
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint8 public a = 200;
+    uint16 public b = 1000;
+    uint256 public sum = a + b;
+    
+}
