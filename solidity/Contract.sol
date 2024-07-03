13af3064abe81042522be970c266cb54b1c17f2d
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-04 00:18:23 +0100

    string literal

--- solidity/Contract.sol
+++ solidity/Contract.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 contract Contract {
     bool public a = true;
     bool public b = false;
-	
 }
 
 // SPDX-License-Identifier: MIT
@@ -14,7 +13,6 @@ contract Contract {
     uint8 public a = 200;
     uint16 public b = 1000;
     uint256 public sum = a + b;
-    
 }
 
 // SPDX-License-Identifier: MIT
@@ -23,5 +21,14 @@ pragma solidity ^0.8.20;
 contract Contract {
     int8 public a = 30;
     int8 public b = -10;
-    int16 public difference = a -b
+    int16 public difference = a - b;
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	bytes32 public msg1 = "Hello World";
+    string public msg2 = "Lord Ranchoatos here crushing solidity powered by alchemy university!!!";
+}
+
