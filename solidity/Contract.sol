5f689305bf0e26c80310a8d8e09790e9df4ec11a
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-04 14:23:40 +0100

    function overload

--- solidity/Contract.sol
+++ solidity/Contract.sol
@@ -106,3 +106,16 @@ contract Contract {
         return x*2;
     }
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    function double(uint x) external pure returns(uint){
+        return x*2;
+    }
+
+    function double(uint x, uint y) external pure returns(uint, uint){
+        return (x*2, y*2);
+    }
+}
