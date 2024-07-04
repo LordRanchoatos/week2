7da57d341232235a76aa527a6db23e5a6c73ec46
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-04 11:08:29 +0100

    returns and view in a function

--- solidity/Contract.sol
+++ solidity/Contract.sol
@@ -67,3 +67,21 @@ contract Contract {
         x = x+1;
     }
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint public x;
+    constructor(uint _x){
+        x = _x;
+    }
+
+    function increment() external{
+        x = x+1;
+    }
+
+    function add(uint a) external returns(uint){
+        return x + a;
+    }
+}
