5f6f9f19c3c3b1ed4f989c6fd54235a178324277
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-06 02:06:57 +0100

    Transfer Tips

--- solidity/addressInteractions/Contract.sol
+++ solidity/addressInteractions/Contract.sol
@@ -25,3 +25,23 @@ contract Contract {
     }
     
 }
+
+//tip the owner of the contract
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
+
+    fallback() external payable {
+
+    }
+
+    function tip()external payable{
+        owner.call{ value: msg.value }("");
+    }
+    
+}
