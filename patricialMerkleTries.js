8ca832675e79e44c8d7aaa0c46a89a47e9736e08
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-07 13:58:23 +0100

    inserting

--- patricialMerkleTries.js
+++ patricialMerkleTries.js
@@ -21,12 +21,24 @@ class Trie {
 
 module.exports = Trie;
 
-class TrieNode {
-    constructor(key) {
-        this.key = key,
-        this.children = {},
-        this.isWord = false
+const TrieNode = require('./TrieNode');
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode(null);
+    }
+    insert(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            node.children[word[i]] = new TrieNode(word[i]);
+            node = node.children[word[i]];
+
+            if (i == word.length - 1) {
+                node.isWord = true;
+            }
+        }
     }
 }
 
-module.exports = TrieNode;
+module.exports = Trie;
