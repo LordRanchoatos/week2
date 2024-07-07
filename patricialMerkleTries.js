895bec306fa0043a1a90171ece3a0168c92c1393
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-07 21:53:56 +0100

    insert branching

--- patricialMerkleTries.js
+++ patricialMerkleTries.js
@@ -42,3 +42,39 @@ class Trie {
 }
 
 module.exports = Trie;
+
+//insert branching 
+class TrieNode {
+    constructor(key) {
+        this.key = key;
+        this.children = {};
+        this.isWord = false;
+    }
+}
+
+module.exports = TrieNode;
+
+const TrieNode = require('./TrieNode');
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode(null);
+    }
+    insert(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            if (!node.children[word[i]]) {
+                node.children[word[i]] = new TrieNode(word[i]);
+            }
+
+            node = node.children[word[i]];
+
+            if (i == word.length - 1) {
+                node.isWord = true;
+            }
+        }
+    }
+}
+
+module.exports = Trie;
