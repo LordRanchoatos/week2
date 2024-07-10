609b6446b4c8fc98efb1afe017d254906e50c4fa
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-10 22:25:34 +0100

    restricting by address

--- solidity/addressInteractions/revertContract.sol
+++ solidity/addressInteractions/revertContract.sol
@@ -6,3 +6,23 @@ contract Contract {
 		require(msg.value >= 1 ether, "Not enough ether sent");
 	}
 }
+
+// Create a public function withdraw that will withdraw all funds from the contract and send them to the deployer of the contract.
+// Require that only the deployer of the contract be allowed to call this function. For all other addresses, this function should revert.
+// 💡 The deployer of the contract is msg.sender of the constructor.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	address owner;
+
+	constructor() payable {
+		owner = msg.sender;
+		require(msg.value >= 1 ether, "Not enough ether sent");
+	}
+
+	function withdraw() public {
+		require(msg.sender == owner);
+		payable(msg.sender).transfer(address(this).balance);
+	}
+}
