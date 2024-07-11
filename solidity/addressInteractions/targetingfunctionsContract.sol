3739c2a1d036d95a881fb758b614c05ee4508a25
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-11 10:24:07 +0100

    abitrary alert

--- solidity/addressInteractions/targetingfunctionsContract.sol
+++ solidity/addressInteractions/targetingfunctionsContract.sol
@@ -55,3 +55,15 @@ contract Sidekick {
         require(success);
     }
 }
+
+//abitrary alert
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function relay(address hero, bytes memory data) external {
+        (bool success, ) = hero.call(data);
+
+        require(success);
+    }
+}
