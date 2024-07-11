d26173882d70cc8dbbed9a4c5073828a965a130f
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-11 17:16:07 +0100

    transfer the contract balance to the beneficiary

--- solidity/addressInteractions/escrowContract.sol
+++ solidity/addressInteractions/escrowContract.sol
@@ -2,14 +2,18 @@
 pragma solidity 0.8.20;
 
 contract Escrow {
-    address public depositor;
-    address public beneficiary;
-    address public arbiter;
+	address public arbiter;
+	address public beneficiary;
+	address public depositor;
 
-    constructor(address a, address b) payable {
-        arbiter = a;
-        beneficiary = b;
-        depositor = msg.sender;
-    }
-    
+	constructor(address _arbiter, address _beneficiary) payable {
+		arbiter = _arbiter;
+		beneficiary = _beneficiary;
+		depositor = msg.sender;
+	}
+
+	function approve() external {
+		(bool success, ) = beneficiary.call{ value: address(this).balance }("");
+		require(success);
+	}
 }
