666c6bee688cc4bd49ded145ab87846cb7751a64
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-11 10:21:09 +0100

    calldata with signature

--- solidity/addressInteractions/targetingfunctionsContract.sol
+++ solidity/addressInteractions/targetingfunctionsContract.sol
@@ -41,3 +41,17 @@ contract Hero {
         alerted = true;
     }
 }
+
+//alldata with signature.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function sendAlert(address hero, uint enemies, bool armed) external {
+        (bool success, ) = hero.call(
+            abi.encodeWithSignature("alert(uint256,bool)", enemies, armed)
+        );
+
+        require(success);
+    }
+}
