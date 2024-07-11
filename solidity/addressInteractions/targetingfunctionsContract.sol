01193ab4a226d62bd915eb04d8ee8122b222fea8
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-11 10:26:23 +0100

    fallback

--- solidity/addressInteractions/targetingfunctionsContract.sol
+++ solidity/addressInteractions/targetingfunctionsContract.sol
@@ -67,3 +67,18 @@ contract Sidekick {
         require(success);
     }
 }
+
+//trige fallback
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function makeContact(address hero) external {
+        // send over any calldata that doesnt match existing signatures!
+        (bool success, ) = hero.call(
+            abi.encodeWithSignature("")
+        );
+
+        require(success);
+    }
+}
