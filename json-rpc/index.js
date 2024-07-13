1dad8ffd5d6f5ebe2ade61cdaf717a7653286c74
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-13 14:29:50 +0100

    get Nonce

--- json-rpc/index.js
+++ json-rpc/index.js
@@ -47,3 +47,22 @@ async function getBalance(address) {
 }
 
 module.exports = getBalance;
+
+async function getNonce(address) {
+    const response = await axios.post(url, {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "eth_getTransactionCount", // <-- fill in the method
+        params: [
+            address,
+            "latest"] // <-- fill in the params
+    });
+    
+    // use this if you want to inspect the response data!
+    console.log(response.data);
+
+    // return the nonce for the address
+    return response.data.result
+}
+
+module.exports = getNonce;
