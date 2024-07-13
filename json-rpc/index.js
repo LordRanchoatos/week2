7122f74393c57e5baf547ca5ec8ce84c8d5453e7
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-13 15:30:57 +0100

    batch transaction

--- json-rpc/index.js
+++ json-rpc/index.js
@@ -86,3 +86,24 @@ async function getTotalTransactions(blockNumber) {
 }
 
 module.exports = getTotalTransactions;
+
+async function getTotalBalance(addresses) {
+    const batch = addresses.map((addr, i) => ({
+        jsonrpc: "2.0",
+        id: i,
+        method: "eth_getBalance",
+        params: [
+            addr,
+        ]
+    })) 
+
+    const response = await axios.post(url, batch);
+    
+    // use this if you want to inspect the response data!
+    console.log(response.data);
+    
+    // return the total balance of all the addresses 
+    return response.data.reduce((a, c) => a + parseInt(c.result), 0)
+}
+
+module.exports = getTotalBalance;
