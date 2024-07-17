81cd3904724ada12e64579e513c879ed7c8ff71c
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-17 19:06:55 +0100

    stack club. add new member

--- week3/solidity/Arrays.sol
+++ week3/solidity/Arrays.sol
@@ -256,3 +256,35 @@ contract Contract {
     }
 }
 
+
+// Stack Club
+// This StackClub contract will have many members like a club or an organization would. 
+// We'll track these members by keeping a list of addresses.
+
+// Members will be added by pushing their address to the top of the list. 
+// Members will be removed by popping the most recent one off of the list. A Last-In-First-Out structure, just like a stack!
+
+// 🏁 Your Goal: Add Members 👨👩
+// Create a dynamic sized array of addresses called members
+// Create an external function addMember which has a single parameter: an address for a new member. Add this address to the members array.
+// Create a public view function isMember that takes an address and returns a bool indicating whether the address is a member or not.
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract StackClub {
+	address[] members; 
+
+	function isMember(address member) external view returns(bool) {
+		for(uint i = 0; i < members.length; i++) {
+			if(members[i] == member) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function addMember(address member) external {
+		members.push(member);
+	}
+}
