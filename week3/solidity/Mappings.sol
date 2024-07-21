2645ef2c0868fe60695394efbe1457ab9835bde1
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-21 06:12:44 +0100

    mapping removal

--- week3/solidity/Mappings.sol
+++ week3/solidity/Mappings.sol
@@ -198,3 +198,47 @@ contract Contract {
     }
     
 }
+
+// Mapping Removal
+// For arrays, removing elements can be an expensive operation, 
+// especially if you don't want to leave any gaps!
+
+// Let's say you had an array containing the unsigned integer values [1,2,3]. 
+// Removing the first element 1 would mean you would have to shift the values 2 and 3 down one position each. 
+// For a bigger array, this could mean many storage operations. Super expensive! 💰⛽
+
+// 💡 In this case you could use a Linked List where you can "stitch" 
+// the previous node to the next node instead of having to shift elements.
+
+// For mappings, removal is quite simple. For example, in this students mapping:
+
+// mapping(address => bool) students;
+
+// function removeStudent(address addr) external {
+//     students[addr] = false;
+// }
+// ☝️ Simple code! We provide the address to find the memory location and set it to false.
+
+// 🏁 Your Goal: Remove Member
+// Create an external function called removeMember that will take an address and 
+// set its corresponding value in the members mapping to false.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    mapping(address => bool) public members;
+
+    function addMember(address _address) external {
+        members[_address] = true;
+    }
+
+    function isMember(address _address) external view returns(bool) {
+        return members[_address];
+    }
+
+    function removeMember(address _address) external {
+        members[_address] = false;
+    }
+    
+}
