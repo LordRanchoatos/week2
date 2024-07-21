a8fdc96e4e56b6aeb42336e70e61fac16973fb77
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-07-21 07:25:13 +0100

    mapping to struct

--- week3/solidity/Mappings.sol
+++ week3/solidity/Mappings.sol
@@ -242,3 +242,57 @@ contract Contract {
     }
     
 }
+
+// Mapping to Struct
+// Here's where mappings get interesting! We can map to many different types. Let's start with structs:
+
+// contract Market {
+//     // create the Collectible struct
+//     struct Collectible {
+//         address owner;
+//         bool forSale;
+//         uint price;
+//     }
+
+//     // map a uint ID to a Collectible struct
+//     mapping(uint => Collectible) idToCollectible;
+
+//     function purchase(uint _id) external payable {
+//         // find the collectible by the id passed in
+//         Collectible storage collectible = idToCollectible[_id];
+//         // purchase the collectible
+//         require(msg.value >= collectible.price);
+//         collectible.owner = msg.sender;
+//         collectible.forSale = false;
+//     }
+// }
+// ☝️ We are able to lookup the collectible using the uint id passed into purchase. Then we have access to the struct to sell it to its new owner.
+
+// 🏁 Your Goal: User Mapping Tokens
+// Let's create a new token where every new user will receive 100 tokens!
+
+// Create a public mapping called users that will map an address to a User struct.
+// Create an external function called createUser. 
+// This function should create a new user and associate it to the msg.sender address in the users mapping.
+// The balance of the new user should be set to 100 and the isActive boolean should be set to true.
+// 🔒 Contract Security
+// Ensure that the createUser function is called with an address that is not associated with an active user. 
+// Otherwise, revert the call.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	struct User {
+		uint balance;
+		bool isActive;
+	}
+
+	mapping(address => User) public users;
+	
+	function createUser() external {
+		require(!users[msg.sender].isActive);
+		users[msg.sender] = User(100, true);
+	}
+
+}
